063aa8d7a99c98711cd4724aaf84d539140a720f
ngochuyennguyen <huyen.nn224011@sis.hust.edu.vn>
2025-04-22 07:40:23 +0700

    user_dashboard.php

--- user_dashboard.php
+++ user_dashboard.php
@@ -3,8 +3,7 @@
 <!DOCTYPE html>
 <html lang="vi">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Trang Chủ Thư viện</title>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <style>
